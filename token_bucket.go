fef8ac368d8b8377512786df848c5eb93201ddfe
Konstantin Voykov <koykov0@gmail.com>
2024-06-15 17:32:42 +0300

    Token bucket implementation; cover with test.

---
+++ token_bucket.go
@@ -0,0 +1,50 @@
+package rpslimit
+
+import (
+	"context"
+	"time"
+)
+
+type TokenBucket struct {
+	c chan struct{}
+	t *time.Timer
+}
+
+func NewTokenBucket(ctx context.Context, limit uint64) *TokenBucket {
+	l := &TokenBucket{
+		c: make(chan struct{}, limit-1),
+	}
+	for i := 0; i < int(limit-1); i++ {
+		l.c <- struct{}{}
+	}
+
+	dur := time.Duration(time.Second.Nanoseconds() / int64(limit))
+	go l.init(ctx, dur)
+
+	return l
+}
+
+func (l *TokenBucket) Allow() bool {
+	select {
+	case <-l.c:
+		return true
+	default:
+		return false
+	}
+}
+
+func (l *TokenBucket) init(ctx context.Context, dur time.Duration) {
+	l.t = time.NewTimer(dur)
+	for {
+		select {
+		case <-l.t.C:
+			select {
+			case l.c <- struct{}{}:
+			default:
+			}
+		case <-ctx.Done():
+			l.t.Stop()
+			return
+		}
+	}
+}
